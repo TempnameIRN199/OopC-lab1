7d06384b4c6ff0b231b10af91346ad73e835885b
考え-Sueta <103942156+hentuho@users.noreply.github.com>
2022-11-01 16:10:40 +0200

    Update Header.h

--- OopC++ lab1/Header.h
+++ OopC++ lab1/Header.h
@@ -10,9 +10,6 @@
 #include <ctype.h>
 using namespace std;
 
-// реализовать класс студент. необходимо хранить в классе следующие данные: ФИО, дата рождения, контактный телефон, город, страна, название учебного учереждения, место и страна(где находится уч. учереждение), номер группы.
-// реализовать функции-члены класса для ввода данных, вывода данных, реализовать аксессоры для доступа к данным класса.
-
 class Student {
 private:
 	string name;
@@ -27,9 +24,9 @@ private:
 	string universityCountry;
 	string groupNumber;
 public:
-	Student(); // конструктор по умолчанию
-	Student(string name, string surname, string patronymic, string dateOfBirth, string phoneNumber, string city, string country, string university, string universityPlace, string universityCountry, string groupNumber); // конструктор с параметрами
-	~Student(); // деструктор
+	Student(); // ГЄГ®Г­Г±ГІГ°ГіГЄГІГ®Г° ГЇГ® ГіГ¬Г®Г«Г·Г Г­ГЁГѕ
+	Student(string name, string surname, string patronymic, string dateOfBirth, string phoneNumber, string city, string country, string university, string universityPlace, string universityCountry, string groupNumber); // ГЄГ®Г­Г±ГІГ°ГіГЄГІГ®Г° Г± ГЇГ Г°Г Г¬ГҐГІГ°Г Г¬ГЁ
+	~Student(); // Г¤ГҐГ±ГІГ°ГіГЄГІГ®Г°
 	void setName(string name); 
 	void setSurname(string surname);
 	void setPatronymic(string patronymic);
@@ -41,7 +38,7 @@ public:
 	void setUniversityPlace(string universityPlace);
 	void setUniversityCountry(string universityCountry);
 	void setGroupNumber(string groupNumber);
-	string getName(); // аксессоры
+	string getName(); // Г ГЄГ±ГҐГ±Г±Г®Г°Г»
 	string getSurname();
 	string getPatronymic();
 	string getDateOfBirth();
